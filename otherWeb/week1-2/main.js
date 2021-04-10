c7d5f7c429aaa848639f7626e8ddb7d8cd97c83e
karen <karen199910588@gmail.com>
2021-04-10 09:58:41 +0800

    First commit

--- otherWeb/week1-2/main.js
+++ otherWeb/week1-2/main.js
@@ -13,11 +13,23 @@ $(function(){
         
         $("h1").text($("li").eq(randomClickNumber).text())
         
-        if(randomClickNumber == 0)
+        if(randomClickNumber == 0){
             $("#outImg").attr("src","https://pic.pimg.tw/ofeyhong/1601970703-3777845449-g.jpg");
-        else if(randomClickNumber == 1)
+            $("li").eq(randomClickNumber).css("background-color", "skyblue");
+            $("li").eq(1).css("background-color", "white");
+            $("li").eq(2).css("background-color", "white");
+        }
+        else if(randomClickNumber == 1){
             $("#outImg").attr("src","https://www.examservice.com.tw/Content/images/Product/img_product/toeic_t.jpg");
-        else if(randomClickNumber == 2)
+            $("li").eq(randomClickNumber).css("background-color", "yellow");
+            $("li").eq(0).css("background-color", "white");
+            $("li").eq(2).css("background-color", "white");
+        }
+        else if(randomClickNumber == 2){
             $("#outImg").attr("src","https://onlinejudge.org/templates/hm_yaml_2_5/img/ojlogo2.svg.png");
+            $("li").eq(randomClickNumber).css("background-color", "purple");
+            $("li").eq(0).css("background-color", "white");
+            $("li").eq(1).css("background-color", "white");
+        }
     });
 });
